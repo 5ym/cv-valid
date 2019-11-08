d84fc057cf6b2b1f69145c0bad8eb3b086290991
5ym <16yuim@gmail.com>
2019-11-08 15:36:03 +0900

    日本語と英数字のみカウント対象に

--- count.js
+++ count.js
@@ -4,11 +4,11 @@ data = {
     'normal': 0,
     'canny': 0
 };
-fs.readdir('./result', function(err, files){
+fs.readdir('./result', async function(err, files){
     if (err) throw err;
     files.forEach(file=> {
-        data.normal += require('./result/'+file)[0]['fullTextAnnotation']['text'].replace(/\r?\n/g, '').length;
-        data.canny += require('./maskresult/'+file)[0]['fullTextAnnotation']['text'].replace(/\r?\n/g, '').length;
+        data.normal += require('./result/'+file)[0]['fullTextAnnotation']['text'].replace(/\r?\n/g, '').replace(/[^亜-熙ぁ-んァ-ヶ\d]/g, '').length;
+        data.canny += require('./maskresult/'+file)[0]['fullTextAnnotation']['text'].replace(/\r?\n/g, '').replace(/[^亜-熙ぁ-んァ-ヶ\d]/g, '').length;
     });
     fs.writeFile('./count.json', JSON.stringify(data, null, '    '), err=>{if(err) throw err;});
 });
