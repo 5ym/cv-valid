2a4d274f313cc79308dcb6a7033b54c356bfe7d9
5ym <16yuim@gmail.com>
2019-11-08 14:24:39 +0900

    first commit

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,6 @@ COPY package*.json ./
 RUN npm install
 COPY . .
 ENV GOOGLE_APPLICATION_CREDENTIALS /usr/src/app/fancrew-dev-labo-30c840de0531.json
+#ENV GOOGLE_APPLICATION_CREDENTIALS /usr/src/app/action-ff8122b99dfa.json
 
 CMD tail -f /dev/null
--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const vision = require('@google-cloud/vision');
 const client = new vision.ImageAnnotatorClient();
 const fs = require('fs');
 
-fs.readdir('./recipt', function(err, files){
+fs.readdir('./recipt', async (err, files) => {
     if (err) throw err;
-    files.forEach(file=> {
-        client.documentTextDetection('./recipt/'+file).then(response => {
-                fs.writeFile('./result/'+file+'.json', JSON.stringify(response, null, '    '), err=>{if(err) throw err;});
-                console.log('success:recipt/'+file);
-            }).catch(err => {
-                console.error('error:recipt/'+file);
-                console.error(err);
-            });
-        client.documentTextDetection('./maskrecipt/'+file).then(response => {
+    for(let file of files) {
+        await client.documentTextDetection('./recipt/'+file).then(response => {
+            fs.writeFile('./result/'+file+'.json', JSON.stringify(response, null, '    '), err=>{if(err) throw err;});
+            console.log('success:recipt/'+file);
+        }).catch(err => {
+            console.error('error:recipt/'+file);
+            console.error(err);
+        });
+        await client.documentTextDetection('./maskrecipt/'+file).then(response => {
             fs.writeFile('./maskresult/'+file+'.json', JSON.stringify(response, null, '    '), err=>{if(err) throw err;});
             console.log('success:maskresult/'+file);
         }).catch(err => {
             console.error('error:maskresult/'+file);
             console.error(err);
         });
-    });
+    }
 });
