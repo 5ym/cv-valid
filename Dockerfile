1319097eaf4bf57e47d74ad3eddf2b80714b539f
5ym <16yuim@gmail.com>
2019-11-08 12:51:37 +0900

    first commit

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 COPY . .
-ENV GOOGLE_APPLICATION_CREDENTIALS /usr/src/app/action-3670d5950362.json
+ENV GOOGLE_APPLICATION_CREDENTIALS /usr/src/app/fancrew-dev-labo-30c840de0531.json
 
 CMD tail -f /dev/null
--- index.js
+++ index.js
@@ -14,9 +14,9 @@ fs.readdir('./recipt', function(err, files){
             });
         client.documentTextDetection('./maskrecipt/'+file).then(response => {
             fs.writeFile('./maskresult/'+file+'.json', JSON.stringify(response, null, '    '), err=>{if(err) throw err;});
-            console.log('success:recipt/'+file);
+            console.log('success:maskresult/'+file);
         }).catch(err => {
-            console.error('error:recipt/'+file);
+            console.error('error:maskresult/'+file);
             console.error(err);
         });
     });
